21ec5824e00511a41ebdeba78362830a3cf50459
Hamza Khuswan <hamzakhuswan@outlook.com>
2021-05-31 09:24:11 +0200

    🔼 update example

--- test/app.tsx
+++ test/app.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import About from "./routes/about";
 import Home from "./routes/home";
 import Posts from "./routes/posts";
+import Profile from "./routes/profile";
 
 export default function App(): React.ReactElement {
     return (
@@ -19,6 +20,14 @@ export default function App(): React.ReactElement {
                 {
                     path: "/posts/:userId/:postId",
                     component: Posts,
+                },
+                {
+                    path: "/profile",
+                    component: Profile,
+                    condition: false,
+                    loading: false,
+                    loadingComponent: <div>Loading</div>,
+                    redirectPath: "/about",
                 },
             ]}
             fallback={<div>404 - page not found</div>}
